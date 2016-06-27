9ad01252108290eb8b8d51c03ac4159645d3b840
Scott Bromander <scott@primeacademy.io>
2016-06-27 14:17:09 -0500

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+	private PlayerInventoryDisplay playerInventoryDisplay;
+	private List<Pickup> inventory = new List<Pickup> ();
+
+	// Use this for initialization
+	void Start () {
+		playerInventoryDisplay = GetComponent<PlayerInventoryDisplay> ();
+		playerInventoryDisplay.OnChangeInventory (inventory);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.CompareTag("Pickup")){
+			Pickup item = hit.GetComponent<Pickup> ();
+			inventory.Add (item);
+			playerInventoryDisplay.OnChangeInventory (inventory);
+			Destroy (hit.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PlayerInventoryDisplay : MonoBehaviour {
+	public Text inventoryText;
+
+	public void OnChangeInventory(List<Pickup> inventory){
+		inventory.Sort (
+			delegate(Pickup p1, Pickup p2) {
+				return p1.description.CompareTo(p2.description);
+			}
+		);
+
+		inventoryText.text = " ";
+
+		string newInventoryText = "Carrying: ";
+		int numItems = inventory.Count;
+		for (var i = 0; i < numItems; i++) {
+			string description = inventory [i].description;
+			newInventoryText += " [" + description + "] ";
+		}
+
+		if (numItems < 1)
+			newInventoryText = "(Empty Inventory)";
+
+		inventoryText.text = newInventoryText;
+	}
+}
